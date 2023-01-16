362a4ce897375f2c276d71b54209c6f46908e76b
JIHONG KIM <64270116+ghk0409@users.noreply.github.com>
2023-01-16 22:50:39 +0900

    non-nullable 기능
    - 느낌표를 추가해서 non-nullable 속성 설정
    - type이나 return 값 등 필수 여부, null 가능 여부 설정에 사용

--- server.js
+++ server.js
@@ -5,20 +5,22 @@ import { ApolloServer, gql } from "apollo-server";
 // GET /films
 
 // graphql schema
+// type에 느낌표!를 추가하여 Non-null 설정
 const typeDefs = gql`
     type User {
         id: ID!
         username: String!
+        firstName: String!
+        lastName: String
     }
-
     type Tweet {
         id: ID!
         text: String!
         author: User!
     }
     type Query {
-        allTweets: [Tweet!]
-        tweet(id: ID!): Tweet!
+        allTweets: [Tweet!]!
+        tweet(id: ID!): Tweet
     }
     type Mutation {
         postTweet(text: String!, userId: ID!): Tweet!
