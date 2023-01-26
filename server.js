37df065cfcd3daafa853b261ad43e5b6725d3273
JIHONG KIM <64270116+ghk0409@users.noreply.github.com>
2023-01-26 20:56:10 +0900

    Resolver 함수 추가 기능
    - join과 같은 relation 방법
    - documentation 방법
    - type resolver 설정

--- server.js
+++ server.js
@@ -10,22 +10,25 @@ let tweets = [
     {
         id: "1",
         text: "hello",
-        author: {
-            id: "5112",
-            username: "Ongs",
-            firstName: "O",
-            lastName: "ngs",
-        },
+        userId: "2",
     },
     {
         id: "2",
         text: "nice2meetU",
-        author: {
-            id: "5112",
-            username: "Ongs",
-            firstName: "O",
-            lastName: "ngs",
-        },
+        userId: "1",
+    },
+];
+
+let users = [
+    {
+        id: "1",
+        firstName: "Ong",
+        lastName: "HHHH",
+    },
+    {
+        id: "2",
+        firstName: "secondeOng",
+        lastName: "holymoly",
     },
 ];
 
@@ -34,21 +37,37 @@ let tweets = [
 const typeDefs = gql`
     type User {
         id: ID!
-        username: String!
         firstName: String!
         lastName: String
+        """
+        this is the sum of firstName + lastName as a String
+        """
+        fullName: String!
     }
+    """
+    Tweet Object represents a resource for a Tweet!
+    """
     type Tweet {
         id: ID!
         text: String!
         author: User!
     }
     type Query {
+        allUsers: [User!]!
         allTweets: [Tweet!]!
         tweet(id: ID!): Tweet
     }
     type Mutation {
-        postTweet(text: String!, userId: ID!): Tweet!
+        postTweet(
+            """
+            text는 반드시 String 타입
+            """
+            text: String!
+            userId: ID!
+        ): Tweet!
+        """
+        Deletes a Tweet, if found, else returns false
+        """
         deleteTweet(id: ID!): Boolean!
     }
 `;
@@ -61,22 +80,26 @@ const resolvers = {
             return tweets;
         },
         // arguments의 첫 번째 인자는 root 고정임
-        tweet(_, { id }) {
+        tweet(root, { id }) {
             // arguments를 받아서 DB 처리 진행 (SQL이나 NoSQL 등 필요 기능을 진행하면됨)
             return tweets.find((tweet) => tweet.id === id);
+        },
+        allUsers(root) {
+            return users;
         },
     },
     Mutation: {
         postTweet(_, { text, userId }) {
+            // userId에 해당하는 user가 존재하는지 체크
+            const check = users.find((user) => user.id === userId);
+            // 존재할 경우, tweet 생성 | 존재하지 않을 경우, 에러 메시지 출력
+            if (!check) {
+                throw new Error("Please Check userId, userId not exist!");
+            }
             const newTweet = {
                 id: tweets.length + 1,
                 text,
-                author: {
-                    id: userId,
-                    userName: "test",
-                    firstName: "te",
-                    lastName: "st",
-                },
+                userId,
             };
             tweets.push(newTweet);
             return newTweet;
@@ -87,6 +110,18 @@ const resolvers = {
             if (!tweet) return false;
             tweets = tweets.filter((tweet) => tweet.id !== id);
             return true;
+        },
+    },
+    User: {
+        // 첫 번째 인자인 root 안에 User 객체가 담겨있음
+        fullName({ firstName, lastName }) {
+            return `${firstName} ${lastName}`;
+        },
+    },
+    Tweet: {
+        author({ userId }) {
+            // Tweet의 userId에 해당하는 user 데이터 가져오기 (join)
+            return users.find((user) => user.id === userId);
         },
     },
 };
